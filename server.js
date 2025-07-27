09f7e39e7c293ef33edd96ffff31ddaf097185a6
Satyajit Gain <satyajitgain.personal@gmail.com>
2025-07-28 05:23:40 +0530

    change package.json

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const server = express();
 const path = require('path');
-
 server.get('/', (req, res) => {
+  res.send('Fontend Comming Soon');
+});
+
+server.get('/srkmovies', (req, res) => {
   res.sendFile(path.join(__dirname, '/srkMovies.json'));
 });
+
 const PORT = process.env.PORT || 5656;
 server.listen(PORT, () => {
   console.log('Server Running at Port Number 5656');
